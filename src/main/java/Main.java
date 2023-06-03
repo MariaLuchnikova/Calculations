27a9c8d118bc5e0b152479595e1fbdf2d9130438
MariaLuchnikova <luchnikova80@gmail.com>
2023-06-03 18:35:20 +0800

    added calculation

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        // с использованием классов-обёрток примитивных дипов данных
+        System.out.println(Integer.MIN_VALUE + "  " + Integer.MAX_VALUE); // диапазон значений данных типа int
+        System.out.println(Float.MIN_VALUE + "  " + Float.MAX_VALUE);    // диапазон значений данных типа float
+        System.out.println(Double.MIN_VALUE + "  " + Double.MAX_VALUE);  // диапазон значений данных типа double
+
+        System.out.println(Integer.MIN_VALUE - 1);
+
+        System.out.println(!true && !false);
+        System.out.println(true && false);
+        System.out.println(false || true);
+        System.out.println(!(10 > 10.55));
+        System.out.println(Boolean.compare(true, false));
+
+        System.out.println(10 / 3);
+        System.out.println(10 + 10.55);
+        System.out.println(10.55 - 10);
+        System.out.println(10 * 10.55);
+        System.out.println(10.55 / 10);
+        System.out.println(10 % 10.55);
+    }
+}
+
+
+
